b6b857789644d163257ada55a982ea97b502f1ef
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-20 13:42:45 +0530

    Form Design Completed

--- routes/product.js
+++ routes/product.js
@@ -6,4 +6,8 @@ router.get("/product", function (req, res, next) {
   res.render("productInterface");
 });
 
+router.post("/product/submitted", function (req, res) {
+  res.render("index");
+});
+
 module.exports = router;
