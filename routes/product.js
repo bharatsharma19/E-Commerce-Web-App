c38ebf3b125fdeea4ba693dd9ae3812c089f4b49
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-22 20:50:17 +0530

    Sql Error

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,36 @@ router.get("/product", function (req, res, next) {
 });
 
 router.post("/product/submitted", function (req, res) {
-  res.render("index");
+  pool.query(
+    "insert into products(categoryid, subcategoryid, brandid, productname, price, offerprice, rating, description, stock, status) values(?,?,?,?,?,?,?,?,?,?)",
+    [
+      req.body.categoryid,
+      req.body.subcategoryid,
+      req.body.brandid,
+      req.body.productname,
+      req.body.price,
+      req.body.offerprice,
+      req.body.rating,
+      req.body.description,
+      req.body.stock,
+      req.body.status,
+    ],
+    function (error, result) {
+      if (error) {
+        console.log("Error : ", error);
+      } else {
+        console.log("Result : ", result);
+      }
+    }
+  );
+
+  if (pool) {
+    console.log("Success");
+  } else {
+    console.log("Error");
+  }
+  
+  res.render("productInterface");
 });
 
 module.exports = router;
