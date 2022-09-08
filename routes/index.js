8c0bb90f639b4d6df81811cf5698752fb705db15
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-09-08 16:53:53 +0530

    Login Page Created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+var pool = require("./pool");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Developed by Bharat Sharma" });
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login", { msg: "" });
 });
 
 module.exports = router;
