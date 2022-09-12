c93a80c55b265fb7cc1a374805594a0d96cfd8f6
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-09-12 17:17:33 +0530

    12 Sep 2022

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var app = express();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productRouter = require("./routes/product");
-
-var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
